ce9107cbb6e1a63ab57aed15ea8bf13d28941b6a
jbx <jbxshare@163.com>
2021-04-28 11:42:38 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,8 @@
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: jbx
+ * @Date: 2021-04-28 11:40:35
+-->
+## test-jiangbx-pkg
+- 测试 npm 发包
---
+++ src/index.js
@@ -0,0 +1,9 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: jbx
+ * @Date: 2021-04-27 15:58:20
+ */
+!function() {
+   console.log(11111); 
+}()
